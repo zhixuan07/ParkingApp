33a2d3f8d1c0b1cb41bc0e7e8a787488955a7282
traceycccc <chinghuey07chow@gmail.com>
2023-11-04 14:52:56 +0800

    Adding function for vehicle report
    Added function for the vehicle report feature

--- app/src/main/java/com/example/parkingappinti/ReportFragment.java
+++ app/src/main/java/com/example/parkingappinti/ReportFragment.java
@@ -1,20 +1,53 @@
 package com.example.parkingappinti;
 
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import es.dmoral.toasty.Toasty;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ReportFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ReportFragment extends Fragment {
+public class ReportFragment extends Fragment implements ReportDialog.DialogFragmentListener{
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String userID;
+    FloatingActionButton fab;
 
+    UUID randomUUID = UUID.randomUUID();
+    String uuid = randomUUID.toString();
+    Date currentDate = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy ");
+    String formattedDate = sdf.format(currentDate);
+
+    private FirebaseRecyclerAdapter<Report,reportViewHolder> adapter;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -60,5 +93,86 @@ public class ReportFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_report, container, false);
+
+    }
+    @Override
+    public void  onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        RecyclerView recyclerView = view.findViewById(R.id.report_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        userID = user.getUid();
+        // retrieve the list of report from firebase and dump into recycle view
+        FirebaseRecyclerOptions<Report> options = new FirebaseRecyclerOptions.Builder<Report>()
+                .setQuery(databaseReference.child("Report").orderByChild("user_id").equalTo(userID), Report.class)
+                .build();
+        adapter = new FirebaseRecyclerAdapter<Report, reportViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull reportViewHolder holder, int position, @NonNull Report model) {
+                holder.bind(model);
+            }
+
+            @NonNull
+            @Override
+            public reportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                // create the report card view bind with the recycle list to
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.report_card, parent, false);
+                return new reportViewHolder(view);
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        // floating action button to open report dialog
+        fab = view.findViewById(R.id.fab);
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        fab.setOnClickListener(v -> {
+            ReportDialog reportDialog = new ReportDialog();
+            reportDialog.setListener(this);
+            reportDialog.show(fragmentManager,"Report");
+
+        });
+
+    }
+    public void submitReport(String plate_number,String issue,String issue_details){
+        // insert the report information to firebase
+        userID =  user.getUid();
+        Report report = new Report(plate_number,issue,issue_details,userID,formattedDate);
+        databaseReference.child("Report").child(uuid).setValue(report);
+        Toasty.success(getContext(),"Report Submitted",Toasty.LENGTH_SHORT).show();
+
+
+    }
+    private  void validateVehicle(String plate_number){
+
+    }
+    // always listen to the adapter whether have new data or data is deleted
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void applyTexts(String plate, String issue, String issue_details) {
+        // from the report dialog receive the inputs and apply on the logic
+        databaseReference.child("Vehicle").child(plate).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    submitReport(plate,issue,issue_details);
+                }else{
+                    Toasty.error(getActivity(),"Vehicle not registered in system",Toasty.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
